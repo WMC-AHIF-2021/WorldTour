bc56ab0781462b7de92b80f7945825a320f22ff1
julia <86598308+julia-meyr@users.noreply.github.com>
2022-02-07 20:55:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+
 window.addEventListener("load", () => {
   const preload = document.querySelector(".preload");
-  setTimeout(500000);
+  delay(10);
   preload.classList.add("preload-finish");
 });
+
+function delay(n){
+  return new Promise(function(resolve){
+      setTimeout(resolve,n*1000);
+  });
+}
