75612f6c6c520899191a8317df2a20a51c2b67c8
Amina <86598145+amina-gbc@users.noreply.github.com>
2022-06-03 09:00:19 +0200

    Add files via upload

--- WorldInData/src/scripts/script.ts
+++ WorldInData/src/scripts/script.ts
@@ -1,10 +1,18 @@
 let picName = [];
-let counter = 0;
+let counter: number = 0;
 let MAX_RUNTIMES: number;
-let content: HTMLElement;
+let content: HTMLElement; //HTMLElement is also a data type
 let progressBar: HTMLElement;
 
+let descriptionHeader: HTMLElement;
+let descriptionParagraph: HTMLElement;
+let descriptionImage: HTMLElement;
+
+let isPaused: boolean = true;
+let intervalID: number;
+
 function fetchData(): void {
+
     fetch("picName.json")
         .then((res) => {
             return res.json();
@@ -20,18 +28,28 @@ function setSimulator(): void {
     content = document.getElementById("picContent");
     content.innerHTML = `<img class="show" id="${picName[counter].id}" alt="${picName[counter].name}" src="img/data/${picName[counter].name}.svg"/>`;
     progressBar = document.getElementById('progress');
+    descriptionHeader = document.getElementById('descriptionHeader');
+    descriptionImage = document.getElementById('descriptionImage');
+    descriptionParagraph = document.getElementById('descriptionParagraph');
 }
 
 function runSimulation(): void{
-    const intervalID = setInterval(() => {
-        if (++counter === MAX_RUNTIMES){
-            window.clearInterval(intervalID);
+    isPaused = !isPaused;
+    intervalID = setInterval(() => {
+        if (!isPaused) {
+            if (++counter === MAX_RUNTIMES){
+                window.clearInterval(intervalID);
+            }
+            console.log(counter);
+            let pic = document.getElementById(counter.toString()) as HTMLImageElement;
+            pic.src = `img/data/${picName[counter].name}.svg`;
+            pic.id = `${picName[counter].id}`;
+            progressBar.style.width = `${100 / (MAX_RUNTIMES) * counter}%`;
+
+            descriptionHeader.innerHTML = picName[counter].descriptionHeader;
+            descriptionImage.innerHTML = `<img class="show" id="descriptionImage" alt="a simple image" src="../img/data/population-density1892.jpg"/>`;
+            descriptionParagraph = picName[counter].description;
         }
-        console.log(counter);
-        let pic = document.getElementById(counter.toString()) as HTMLImageElement;
-        pic.src = `img/data/${picName[counter].name}.svg`;
-        pic.id = `${picName[counter].id}`;
-        progressBar.style.width = `${100 / (MAX_RUNTIMES) * counter}%`;
     }, 200);
 }
 
@@ -42,5 +60,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
         counter = 0;
         progressBar.style.width = "0%";
         content.innerHTML = `<img class="show" id="${picName[counter].id}" alt="${picName[counter].name}" src="img/data/${picName[counter].name}.svg"/>`;
+        descriptionHeader.innerHTML = picName[counter].descriptionHeader;
+        descriptionImage.innerHTML = `<img class="show" id="descriptionImage" alt="a simple image" src="../img/data/population-density1892.jpg"/>`;
+        descriptionParagraph = picName[counter].description;
+
+        window.clearInterval(intervalID);
     });
 })
