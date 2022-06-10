0043f7c972d54b775cf2c2e978c3e4d750ee1ae6
julia <86598308+julia-meyr@users.noreply.github.com>
2022-06-10 09:30:33 +0200

    Add files via upload

---
+++ Quiz/FlagQuiz/flagquiz.ts
@@ -0,0 +1,188 @@
+const baseUrl = '../../Websites/'
+const quizData = [
+    {
+        flag: baseUrl + 'Africa/images/chad/chad.png',
+        continent: "Africa",
+        country: "chad",
+    },
+    {
+        flag: baseUrl + 'Asia/images/oman/flag.jpg',
+        continent: "Asia",
+        country: "oman",
+    },
+    {
+        flag: baseUrl + 'Europe/images/Moldova/Flag.png',
+        continent: "Europe",
+        country: "moldova",
+    },
+    {
+        flag: baseUrl + 'Africa/images/namibia/namibia.png',
+        continent: "Africa",
+        country: "namibia",
+    },
+    {
+        flag: baseUrl + 'Africa/images/zambia/zambia.png',
+        continent: "Africa",
+        country: "zambia",
+    },
+    {
+        flag: baseUrl + 'Asia/images/bahrain/flag.png',
+        continent: "Asia",
+        country: "bahrain",
+    },
+
+    {
+        flag: baseUrl + 'SouthAmerica/images/peru/peru.jpg',
+        continent: "South America",
+        country: "peru",
+    },
+    {
+        flag: baseUrl + 'NorthAmerica/images/Canada/Flag.png',
+        continent: "North America",
+        country: "canada",
+    },
+    {
+        flag: baseUrl + 'Asia/images/china/flag.jpg',
+        continent: "Asia",
+        country: "china",
+    },
+    {
+        flag: baseUrl + 'Asia/images/thailand/flag.jpg',
+        continent: "Asia",
+        country: "thailand",
+    },
+
+    {
+        flag: baseUrl + 'SouthAmerica/images/brazil/brazil.png',
+        continent: "South America",
+        country: "brazil",
+    },
+    {
+        flag: baseUrl + 'Australia/images/fiji/flag.jpg',
+        continent: "Australia",
+        country: "fiji",
+    },
+    {
+        flag: baseUrl + 'Europe/images/spain/Flag.png',
+        continent: "Europe",
+        country: "spain",
+    },
+    {
+        flag: baseUrl + 'Europe/images/Andorra/Flag.png',
+        continent: "Europe",
+        country: "andorra",
+    },
+    {
+        flag: baseUrl + 'NorthAmerica/images/Haiti/Flag.png',
+        continent: "North America",
+        country: "haiti",
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const questionEl = document.getElementById('question');
+const submitBtn = document.getElementById('submitCountry');
+const hintBtn = document.getElementById('hintBtn');
+const hintEl = document.getElementById('hintOutPut');
+const outPutEl = document.getElementById('output');
+const input = <HTMLInputElement>document.getElementById('sizeIn');
+
+let currentQuiz:number = 0;
+let score:number = 0;
+let index: number = 0;
+let questionsAsked = [quizData.length];
+
+function WasQuestionAsked() {
+    for (let i = 0; i < questionsAsked.length; i++) {
+        if(questionsAsked[i] === currentQuiz){
+            return true;
+        }
+    }
+    return false;
+}
+
+function loadImage():Promise<void> {
+    generateNewFlagToQuiz();
+    let QuestionAlreadyAsked = WasQuestionAsked();
+    if(QuestionAlreadyAsked) {
+        while(QuestionAlreadyAsked && index < quizData.length) {
+            generateNewFlagToQuiz();
+            QuestionAlreadyAsked = WasQuestionAsked();
+        }
+    }
+    questionsAsked[index] = currentQuiz;
+    console.log(currentQuiz);
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerHTML = '';
+    if(index === 0) {
+        questionEl.innerHTML = '<div class="flagGuessingHeader">' +
+            '<a class="quizName textcolor" style="text-align: right">Flag Guessing</a>'
+            + '</div>';
+    }
+    else {
+        questionEl.innerHTML = '<div class="flagGuessingHeader">' +
+            '<a class="quizName textcolor">Flag Guessing</a>' +
+            '<a class="points" style="text-align:right">Score: ' + score + '/' + index + '</a>'
+            + '</div>';
+    }
+    questionEl.innerHTML +=  '<img src=' + currentQuizData.flag + '>';
+    return;
+}
+
+function getCountry():string {
+    return input.value.toLowerCase();
+}
+
+function isCountryRight(guess:string) : boolean{
+    if(guess === quizData[currentQuiz].country) {
+        return true;
+    }
+    return false;
+}
+
+function end() {
+    quiz.innerHTML = `<h2>You guessed ${score}/${quizData.length} countries by their flag correctly!</h2>
+           <button style="reloadButton" onclick="location.reload()">Reload</button>`;
+}
+
+function generateNewFlagToQuiz() {
+    currentQuiz = Math.floor(Math.random() * quizData.length);
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadImage();
+});
+
+submitBtn.addEventListener('click', () => {
+    const guessedCountry = getCountry();
+    if(isCountryRight(guessedCountry)) {
+        score++;
+        outPutEl.innerHTML = `<p>Nice, you got it <span class="right">right</span>!<br>`;
+    }
+    else {
+        outPutEl.innerHTML = `<p>Sorry, that\'s <span class="wrong">wrong</span> ...<br>`;
+    }
+    index++;
+    hintEl.innerHTML = '';
+    if(index > quizData.length - 1) {
+        end();
+    }
+    else {
+        //console.log(currentQuiz);
+        setTimeout(async () => {
+            await loadImage();
+            outPutEl.innerHTML = '';
+            loadImage();
+        }, 1500);
+    }
+})
+
+let hintButtonClickCounter:number = 0;
+hintBtn.addEventListener('click', () => {
+    hintButtonClickCounter++;
+    if(hintButtonClickCounter % 2 !== 0)
+        hintEl.innerHTML = `<p class="hint">This country belongs to the continent ${quizData[currentQuiz].continent}.</p>`;
+    else
+        hintEl.innerHTML = '';
+})
