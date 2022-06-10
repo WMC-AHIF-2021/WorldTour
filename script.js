59384f0e073a3d8fc4d50d987237fc1c605e6e10
julia <86598308+julia-meyr@users.noreply.github.com>
2022-06-10 14:08:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function delay(n){
       setTimeout(resolve,n*1000);
   });
 }
+
+// für das einfliegende Flugzeug:
+let flyIn = document.querySelector(".flyingPlane");
+let isDone;
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.7,
+};
+
+const callback = function (entries, observer) {
+  let observed = entries[0];
+  if (observed.isIntersecting) {
+    flyIn.classList.add ("slideright");
+  }
+}
+
+const observer = new IntersectionObserver (callback, options);
+if (flyIn) {
+  observer.observe (flyIn);
+}
+
